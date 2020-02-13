2c4fc7a1bd4797d94b9809fe9a8dddfaa0227eb1
WilberCode <parionahuaraca@gmail.com>
2020-02-13 09:14:44 -0500

    cambios

--- templates/views/front.php
+++ templates/views/front.php
@@ -2,48 +2,114 @@
  
     
    <div class="max-w-6xl m-auto  shadow">  
-         <img  class="w-full " src="https://d2fc2l3thsf47o.cloudfront.net/images/e-content-banner.jpg" alt="">   
+         <!-- <img  class="w-full " src="https://d2fc2l3thsf47o.cloudfront.net/images/e-content-banner.jpg" alt="">    -->
        
         <div class="flex">
             <div class="w-2/3">
-                 <h1 class="font-sans" >Contenido Principal 1</h1>
-                <h2  class="font-sans" >Contenido Principal 2</h2>
-                <h3  class="font-sans" >Contenido Principal 3</h3>
-                <h4  class="font-sans" >Contenido Principal 4</h4>     
-                <h5  class="font-sans" >Contenido Principal 5</h5>
+                 <h1 class="" >Contenido Principal 1</h1>
+                <h2  class="" >Contenido Principal 2</h2>
+                <h3  class="" >Contenido Principal 3</h3>
+                <h4  class="" >Contenido Principal 4</h4>     
+                <h5  class="" >Contenido Principal 5</h5>
             </div>
             <div class="w-1/3 p-4">
-                  <article  class="rounded shadow overflow-hidden" > 
-                    <a href="#">
-                    <figure>
-                        <img src="https://i.ytimg.com/vi/x_JbIl9SS0E/maxresdefault.jpg" alt="">
-                    </figure> 
-                    <div  class="p-6" >
-                        <h1>Title</h1>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>Lorem ipsum dolor sit amet. dw</p>
-                    </div>
-                    </a>
+                
+            </div>
+        </div> 
+   </div>     
+   <div class="container h-10"> 
+   </div>  
+   <div class="container m-auto">
+       <div class="grid grid-cols-3 gap-5">
+           <div class="main col-span-2 ">
+            <!-- tabs -->
+            <ul class="tab" >
+                <li class="tab-item" > <a href="#de">Temario</a> </li>
+                <li class="tab-item" > <a href="#">Certificado</a> </li>
+                <li class="tab-item" > <a href="#">Docentes</a> </li>
+                <li class="tab-item" > <a href="#">Inversion</a> </li> 
+            </ul>
+            <!-- tab contenido -->
+            <div class="">
+                <div id="temario">
+
+
+                <?php
+                    // args
+                    $args = array(
+                        'numberposts'	=> -1,
+                        'posts_per_page'   => 2,
+                        'post_type'		=> 'mis_congresos'
+                    ); 
+                    // query
+                    $the_query = new WP_Query( $args );  
+                    if( $the_query->have_posts() ): 
+                        while( $the_query->have_posts() ) :$the_query->the_post(); ?>  
+                                <div class=""> 
+                                <?php 
+                                    if( have_rows('congreso_temario')):  
+                                            while ( have_rows('congreso_temario') ) : the_row();   	
+                                            if( have_rows('congreso_presentacion')):  
+                                                    while ( have_rows('congreso_presentacion') ) : the_row();   	
+                                                        $congreso_presentacion_titulo = get_sub_field('congreso_presentacion_titulo');  
+                                                        $congreso_presentacion_descripcion = get_sub_field('congreso_presentacion_descripcion');
+                                                    ?>
+                                                      <div class="mt-8">
+                                                         <h2> <?php echo $congreso_presentacion_titulo ?> </h2>  
+                                                         <?php echo $congreso_presentacion_descripcion ?> 
+                                                      </div> 
+                                            
+                                                <?php   
+                                                    endwhile; 
+                                            else : 
+                                                    echo '<span class="bg-info">Sin Presentacion</span>'; 
+                                            endif; 
+                                            ?>
+                                                <?php   
+                                                    endwhile; 
+                                            else : 
+                                                    echo '<span class="bg-info">Sin Temario</span>'; 
+                                            endif; 
+                                            ?>
+                                </div>  
+
+
+
+
+
+
+                    <?php endwhile;
+                    else:
+                        printf('<p>No hay entradas</p>');
+                    endif;
+                    rewind_posts();  ?> 
+                 
+
+                </div>  
+            </div>
+           </div>
+            <div class="aside">
+                <article  class="rounded shadow overflow-hidden" > 
+                        <a href="#">
+                        <figure>
+                            <img src="https://i.ytimg.com/vi/x_JbIl9SS0E/maxresdefault.jpg" alt="">
+                        </figure> 
+                        <div  class="p-6" >
+                            <h1>Title</h1>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum dolor sit amet. dw</p>
+                        </div>
+                        </a>
                   </article>
             </div>
-        </div>
-        <div  class="py-4 px-6" > 
-           
-
-            <p  class="mt-0 mb-1" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ut vel placeat accusamus porro soluta molestias impedit quasi suscipit, quod quam pariatur, odit iusto laboriosam ratione repudiandae. Consequuntur, deserunt quisquam.</p>
-            <span  class="text-xs block mb-5 mt-1 italic" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, qui cupiditate recusandae cum cumque dolorum obcaecati facere tempore autem aliquam dolorem? Optio tempore, nihil totam in fugiat suscipit consequatur doloribus.</span>
-            <button class="bg-blue-600 text-white px-6 py-3 rounded " >Registrarse Aqui</button> 
-
-             <div class="w-full shadow rounded flex  overflow-hidden mt-4">
-                <div class="w-1/2 bg-white " > 
-                    <img src="https://i.stack.imgur.com/KZFfe.png" alt="">
-                </div>
-                <div class="w-1/2 bg-white p-6" > 
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus error quos nemo laudantium, placeat consequuntur debitis rem inventore qui unde sed animi itaque rerum eveniet et dolor labore omnis.
-                </div>
-            </div> 
-        </div>
-   </div>     
-   
-   <div class="mb-8" >
+       </div>
    </div>
+   <div class="mb-8"    >
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa esse deleniti error, pariatur, tempore fugiat in commodi voluptatibus cupiditate quia possimus, nulla asperiores! Ratione, alias. Iusto magnam officiis quis quod.</p>
+   </div>
+  
+   <div class="mb-8 h-56"   >
+    </div>
+  
+   <div class="mb-8 h-56"   >
+    </div>
