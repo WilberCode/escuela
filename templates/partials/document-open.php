e716555ede4d929612aeb46a92b98f698bdf3f85
wilber <parionahuaraca@gmail.com>
2022-10-17 21:42:00 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -15,11 +15,13 @@
    <!-- Google tag (gtag.js) -->
    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6K82MHCV6P"></script>
    <script>
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
+       setTimeout(function(){ 
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
 
-   gtag('config', 'G-6K82MHCV6P');
+         gtag('config', 'G-6K82MHCV6P');
+      },3000);
    </script>
  
 </head>
